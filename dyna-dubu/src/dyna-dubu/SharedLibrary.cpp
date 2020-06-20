f71de2b182170775fa4d4bf23ee6b597516c644d
Hussein Taher <hussein_taher@outlook.com>
2020-06-20 03:13:28 +0200

    added ifdef

--- dyna-dubu/src/dyna-dubu/SharedLibrary.cpp
+++ dyna-dubu/src/dyna-dubu/SharedLibrary.cpp
@@ -5,11 +5,19 @@
 namespace dd {
 
 SharedLibrary::SharedLibrary(const char* libraryName) {
-	std::cout << "Constructor" << std::endl;
+#ifdef WIN32
+	std::cout << "Constructor Win32" << std::endl;
+#else
+	std::cout << "Constructor Unix" << std::endl;
+#endif
 }
 
 SharedLibrary::~SharedLibrary() {
-	std::cout << "Destructor" << std::endl;
+#ifdef WIN32
+	std::cout << "Destructor Win32" << std::endl;
+#else
+	std::cout << "Destructor Unix" << std::endl;
+#endif
 }
 
 }  // namespace dd
